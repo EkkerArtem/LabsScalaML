d9cad84f73e7fc5eaf7955d2479ff7551c595c4e
ArtemEkker <ekkerartem@gmail.com>
2019-02-17 18:10:59 +0200

    Lecture 5 part 1 refactored

--- src/main/scala/lecture_5/part1/Empty.scala
+++ src/main/scala/lecture_5/part1/Empty.scala
@@ -1,7 +1,7 @@
 package lecture_5.part1
 
-object Empty extends IntSet {
-  override def incl(x: Int): IntSet = new NonEmpty(x, Empty, Empty)
+class Empty extends IntSet {
+  override def incl(x: Int): IntSet = new NonEmpty(x,new Empty,new Empty)
 
   override def contains(x: Int): Boolean = false
 
